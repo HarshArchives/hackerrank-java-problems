9518e950c8d66ca7e814c007afbbfa150f4ad0dc
Harsh Vardhan Pandey <harshvardhanpandey@Harshs-MacBook-Air.local>
2023-03-27 21:47:52 +0530

    ✨ feat: add PalindromeString problem

---
+++ PalindromeString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PalindromeString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string : ");
+        String A = sc.next();
+        /* Enter your code here. Print output to STDOUT. */
+        String R = new StringBuilder(A).reverse().toString();
+        if (A.equals(R))
+            System.out.println(A + " YES");
+        else
+            System.out.println(A + " NO");
+        sc.close();
+    }
+}
