e4698a9507115a0851e53a5d8fa4e90978825c83
geekyharsh05 <hp424805@gmail.com>
2023-03-28 22:03:43 +0530

    :sparkles: feat: add StdIn StdOut I Problem

---
+++ src/StdInStdOut.java
@@ -0,0 +1,17 @@
+package src;
+import java.util.Scanner;
+
+public class StdInStdOut {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.close();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
