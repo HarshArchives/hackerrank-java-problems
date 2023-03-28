2469e483b6ffee5ab1199c0eda0f90ec64e2654e
geekyharsh05 <hp424805@gmail.com>
2023-03-28 22:07:35 +0530

    :sparkles: feat: add Welcome to Java Problem

---
+++ src/Welcome.java
@@ -0,0 +1,9 @@
+package src;
+public class Welcome {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World.");
+        System.out.println("Hello, Java.");
+    }
+}
+
