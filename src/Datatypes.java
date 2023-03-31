6c6e7a002594cf0b202e5eda45f2e6d3d34153ea
geekyharsh05 <hp424805@gmail.com>
2023-03-31 21:48:08 +0530

    :sparkles: feat: add EndOfFile Problem

---
+++ src/Datatypes.java
@@ -0,0 +1,51 @@
+package src;
+
+import java.util.Scanner;
+
+class Datatypes {
+    public static void main(String []args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++)
+        {
+
+            try
+            {
+                long x=sc.nextLong();
+                System.out.println(x+" can be fitted in:");
+                try{
+                    byte y = (byte) x;
+                    if(y == x)
+                    {
+                        System.out.println("* byte");    
+                    }
+                }catch(Exception e){}
+                try{
+                    short y = (short) x;
+                    if(y == x)
+                    {
+                        System.out.println("* short");
+                    }
+                }catch(Exception e){}
+                try{
+                    int y = (int) x;
+                    if(y == x)
+                    {
+                        System.out.println("* int");
+                    }
+                }catch(Exception e){}
+                
+                
+                
+                System.out.println("* long");
+            }
+            catch(Exception e)
+            {
+                System.out.println(sc.next()+" can't be fitted anywhere.");
+            }
+        }
+        sc.close();
+    }
+}
