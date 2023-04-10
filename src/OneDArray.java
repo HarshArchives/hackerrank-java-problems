163d2f6a1eabbbecf11eac1ae6416b0d2cb9cc93
geekyharsh05 <hp424805@gmail.com>
2023-04-10 08:41:25 +0530

    :sparkles: feat: add One Dimensional Array Problem

---
+++ src/OneDArray.java
@@ -0,0 +1,25 @@
+package src;
+
+import java.util.*;
+
+// {@author -> geekyharsh05}
+
+public class OneDArray {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
